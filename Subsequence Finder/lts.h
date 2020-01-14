13e8db285d97cdc5634b462393e4febcd3dcc5d4
Saheel-Sarker <56662628+Saheel-Sarker@users.noreply.github.com>
2020-01-14 15:56:34 -0700

    Add files via upload

---
+++ Subsequence Finder/lts.h
@@ -0,0 +1,7 @@
+#include <string.h>
+#include <stdio.h>
+#include "lcs.h"
+
+extern char lts[11110];
+void findLts (int length, char seq[]);
+void findLcts (int seq1Len, int seq2Len, char seq1[], char seq2[]);
