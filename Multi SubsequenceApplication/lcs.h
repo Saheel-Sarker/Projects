77e6c656a1786c8f1c2d392693d20a19be02f012
Saheel-Sarker <56662628+Saheel-Sarker@users.noreply.github.com>
2020-01-14 15:51:12 -0700

    Delete lcs.h

--- Multi SubsequenceApplication/lcs.h
+++
@@ -1,6 +0,0 @@
-extern int LcsTable[11110][11110];
-extern int currentIndex;
-extern char lcs[11110];
-
-int makeTable (int seq1Len, int seq2Len, char seq1[], char seq2[]);
-void findLcs(int seq1Len, int seq2Len, char seq[]);
