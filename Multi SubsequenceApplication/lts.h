69f6b638b951cf6309d086a40feae11411917f04
Saheel-Sarker <56662628+Saheel-Sarker@users.noreply.github.com>
2020-01-14 15:51:42 -0700

    Delete lts.h

--- Multi SubsequenceApplication/lts.h
+++
@@ -1,7 +0,0 @@
-#include <string.h>
-#include <stdio.h>
-#include "lcs.h"
-
-extern char lts[11110];
-void findLts (int length, char seq[]);
-void findLcts (int seq1Len, int seq2Len, char seq1[], char seq2[]);
